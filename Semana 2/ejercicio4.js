2f8e3b8e897e091012bea649457ae5247b411465
Leandro Contestabile <83560286+leancontestabile@users.noreply.github.com>
2024-05-24 13:32:05 -0300

    Delete Semana 2 directory

--- Semana 2/ejercicio4.js
+++
@@ -1,8 +0,0 @@
-/*
-
-1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar Cuántos mundiales vivió una persona.
-
-Ten en cuenta que se juega un mundial cada 4 años y que se han jugado 21 copas del mundo.
-
-*/
-
